895a7fbab6a6c909a456818e50a032779c20c12b
Lucas <ga53foy@mytum.de>
2021-01-14 23:48:08 +0100

    very simple exerciseDetail Page

---
+++ javascript/exerciseStorage.js
@@ -0,0 +1,11 @@
+const exercises = [{
+	"name": "Armzieher",
+	"description": "Den Stab mit beiden Händen hinter den Rücken nehmen und nach unten durchstrecken. Die Arme nun langsam nach oben bewegen, bis sie ungefähr im Rechten Winkel zum Rücken stehen. Dies nun 20 Sekunden halten, danach die Arme 10 Sekunden weiter nach oben drücken. Nun die Arme langsam wieder nach unten lassen und 10 Sekunden pausieren. Dies sechsmal wiederholen.",
+	"difficulty": 3,
+	"type": ["Dehnung"],
+	"material": ["Gymnastikstab, Besen o.ä."],
+	"rests": ["Beine", "Bauch"],
+	"trains": ["Arme", "Schultern"],
+	"variants": "Oben und Unten",
+	"time": 5
+}];
