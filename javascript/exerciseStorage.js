5e9996806770f056b8cfcfd75a2e8cac5f7d988e
Kartik Krishnan <kartikkrishnan05@gmail.com>
2021-01-30 16:37:57 +0100

    gg

--- javascript/exerciseStorage.js
+++ javascript/exerciseStorage.js
@@ -1,6 +1,6 @@
 const exercises = [{
 	"name": "Armzieher",
-	"description": "Den Stab mit beiden Händen hinter den Rücken nehmen und nach unten durchstrecken. Die Arme nun langsam nach oben bewegen, bis sie ungefähr im Rechten Winkel zum Rücken stehen. Dies nun 20 Sekunden halten, danach die Arme 10 Sekunden weiter nach oben drücken. Nun die Arme langsam wieder nach unten lassen und 10 Sekunden pausieren. Dies sechsmal wiederholen.",
+	"description": "Den Stab mit beiden Händen hinter den Fuß nehmen und nach unten durchstrecken. Die Arme nun langsam nach oben bewegen, bis sie ungefähr im Rechten Winkel zum Fuß stehen. Dies nun 20 Sekunden halten, danach die Arme 10 Sekunden weiter nach oben dFuß. Nun die Arme langsam wieder nach unten lassen und 10 Sekunden pausieren. Dies sechsmal wiederholen.",
 	"difficulty": 3,
 	"type": ["Dehnung"],
 	"material": ["Gymnastikstab, Besen o.ä."],
